3c272a4db00aae947cc34b60a33de82237e0c61b
Chaitanya Paladugums <nagachaitanyapaladugums@gmail.com>
2024-12-05 15:07:19 -0600

    Coursera-Code is Working!- largest and smallest numbers with indefinite loops along with exceptions.

--- src/coursera/python_for_everybody/assignment_5_2.py
+++ src/coursera/python_for_everybody/assignment_5_2.py
@@ -5,12 +5,28 @@
 # a try/except and put out an appropriate message and ignore the number.
 # Enter 7, 2, bob, 10, and 4 and match the output below.
 
-if __name__=='__main__':
-    largest= None
+if __name__ == '__main__':
+    largest = None
+    smallest = None
     while True:
-        num= int(input('Enter a number: '))
-        if num== done:
+        num = input('Enter a number: ')
+        if num == 'done':
             break
-        elif largest is None or num>largest:
-            largest=num
-    print('The largest number is',largest)
+        else:
+            try:
+                num_float = float(num)
+            except:
+                print('Please enter a valid input number')
+                continue
+            if largest is None:
+                largest = num_float
+                smallest = num_float
+                continue
+            elif num_float > largest:
+                largest = num_float
+            elif num_float < smallest:
+                smallest = num_float
+    if largest is None or smallest is None:
+        print('No number entered till now! So by default it is None')
+    else:
+        print(f'The largest number is {largest}, {smallest}')

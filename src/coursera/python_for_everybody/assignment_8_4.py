6dd1e339b14a3dbd8b4e2ca7ec0041baae34fac8
Chaitanya Paladugums <nagachaitanyapaladugums@gmail.com>
2024-12-10 15:17:14 -0600

    Coursera- Code Working- Creating a list of all unique words present in a file.

--- src/coursera/python_for_everybody/assignment_8_4.py
+++ src/coursera/python_for_everybody/assignment_8_4.py
@@ -3,7 +3,7 @@
 # The program should build a list of words. For each word on each line check to see
 # if the word is already in the list and if not append it to the list.
 # When the program completes, sort and print the resulting words in
-# python sort() order as shown in the desired output.
+# python sort() order as shown in the desired output
 #You can download the sample data at http://www.py4e.com/code3/romeo.txt
 
 if __name__== '__main__':

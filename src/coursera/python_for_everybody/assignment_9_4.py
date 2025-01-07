ac6a7cf96e740028003309e6ab61559f89eb6d21
Chaitanya Paladugums <nagachaitanyapaladugums@gmail.com>
2025-01-07 17:18:22 -0600

    Working code_updated_ Finding the prolific mail senders from a bunch of mail information.

--- src/coursera/python_for_everybody/assignment_9_4.py
+++ src/coursera/python_for_everybody/assignment_9_4.py
@@ -8,8 +8,8 @@
 # After the dictionary is produced, the program reads through the
 # dictionary using a maximum loop to find the most prolific committer.
 
-if __name__== '__main__':
-    fname= input("Enter file name: ")
+if __name__ == '__main__':
+    fname = input("Enter file name: ")
     fhand = open(fname)
     counts = dict()
     for line in fhand:
@@ -17,13 +17,20 @@ if __name__== '__main__':
         #     print(line)
         if not line.startswith('From '):
             continue
-        line_lst= line.split()
-        mail_lst= line_lst[1].split('@')
-        sender_name= mail_lst[0]
-        counts[sender_name]= counts.get(sender_name,0) + 1
-    names_lst= list(counts.keys())
-    counts_lst= list(counts.values())
-    counts_max= sorted(counts_lst, reverse=True)[0]
-    for name, count in counts.items():
-        if count == counts_max:
-            print(f'{name} is the prolific sender with a total of {count} mails')
+        line_lst = line.split()
+        mail_lst = line_lst[1].split('@')
+        sender_name = mail_lst[0]
+        counts[sender_name] = counts.get(sender_name, 0) + 1
+    names_lst = list(counts.keys())
+    counts_lst = list(counts.values())
+    counts_max = max(counts_lst)
+    print(counts_max)
+    print(counts)
+    max_names = [key for key, value in counts.items() if value == counts_max]
+    print(max_names)
+    print(f'{", ".join(max_names)} is/are the prolific sender/s with a total of {counts_max} mails')
+    # counts_max= sorted(counts_lst, reverse=True)[0]
+    # print(counts_max)
+    # for name, count in counts.items():
+    #     if count == counts_max:
+    #         print(f'{name} is the prolific sender with a total of {count} mails')

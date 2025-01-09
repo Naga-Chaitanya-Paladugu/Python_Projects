a4e28d249446e4b1157fe05fb0dad27a73a2edd9
Chaitanya Paladugums <nagachaitanyapaladugums@gmail.com>
2025-01-09 17:32:18 -0600

    Working code_ finding average score from a class of students building those counts into dictionary, split, list comprehension etc.

--- src/hackerrank_practice/finding_percentage.py
+++ src/hackerrank_practice/finding_percentage.py
@@ -4,15 +4,15 @@ def average_score(name):
     print(f"{name}'s average is {student_avg}")
 
 if __name__ == '__main__':
-    #Testing dictionary
-    student_profile= {'ja': [2, 3, 4], 'ka': [4, 5, 6], 'la': [6, 7, 7]}
+    # #Testing dictionary
+    # student_profile= {'ja': [2, 3, 4], 'ka': [4, 5, 6], 'la': [6, 7, 7]}
 
-    # student_profile= {}
-    # for _ in range(int(input('Enter the number of student profiles: '))):
-    #     info = input('Enter the name and scores: ').split()
-    #     for i in range(1):
-    #         student_profile[info[i]] = [int(i) for i in info[i+1: ]]
-    # print(student_profile)
+    student_profile= {}
+    for _ in range(int(input('Enter the number of student profiles: '))):
+        info = input('Enter the name and scores: ').split()
+        for i in range(1):
+            student_profile[info[i]] = [int(i) for i in info[i+1: ]]
+    print(student_profile)
     query_name = input('Enter the name of the student for calculating average score: ')
     average_score(query_name)
 
